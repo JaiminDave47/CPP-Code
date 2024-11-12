0df45de2a118a601c45369fc914b96d9e96ab38e
Jaimin Dave <jaimindave477@gmail.com>
2024-11-12 11:45:05 +0530

    Variable and Datatypes

---
+++ Lecture2/02_datatypes.cpp
@@ -0,0 +1,15 @@
+// Variables & Datatypes
+// int, char, float, bool, double.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 17;
+    float cgpa = 9.8f;
+    char grade = 'A';
+    cout << age << "\n";
+    cout << cgpa << "\n";
+    cout<< grade << "\n";
+    return 0;
+}
