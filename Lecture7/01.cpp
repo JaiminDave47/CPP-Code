ba07441f2d2ff820d1609e0c0c3e00ee62122f4c
Jaimin Dave <jaimindave477@gmail.com>
2025-01-19 16:21:13 +0530

    Bitwise Operators

---
+++ Lecture7/01.cpp
@@ -0,0 +1,18 @@
+// & -> Bitwise AND
+// | -> Bitwise OR
+// ^ -> bitwise XOR
+// << -> Biweise Left-Shift
+// >> -> Bitwise Right-Shift
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 6, b = 10;
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    cout << (a ^ b) << endl;
+    cout << (10 << 2) << endl;
+    cout << (10 >> 1) << endl;
+    return 0;
+}
---
+++ Lecture7/hw1.cpp
@@ -0,0 +1,10 @@
+// Figure out how to find if a number is power of 2 without using any loop
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 5;
+    cout << (a >> 2) << endl;
+    return 0;
+}
